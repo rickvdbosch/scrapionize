b4ae54f9e63b093284484078adb7ddd12d41188d
Rick van den Bosch <rickvdbosch@outlook.com>
2021-04-14 17:31:14 +0200

    Added the Scrapionize project Added the SessionizeData model

---
+++ Scrapionize/Models/SessionizeData.cs
@@ -0,0 +1,25 @@
+namespace Scrapionize.Models
+{
+	public class SessionizeData
+	{
+		public string EventName { get; set; }
+
+		public string EventStartDate { get; set; }
+
+		public string EventEndDate { get; set; }
+
+		public string EventUrl { get; set; }
+
+		public string CfpStartDate { get; set; }
+
+		public string CfpEndDate { get; set; }
+
+		public string Location { get; set; }
+
+		public string Travel { get; set; }
+
+		public string Accomodation { get; set; }
+
+		public string EventFee { get; set; }
+	}
+}
