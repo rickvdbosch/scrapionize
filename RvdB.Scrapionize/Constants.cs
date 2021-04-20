f4d8b98910b4ef039fdcf96eb8c5c4b20bee8ff9
Rick van den Bosch <rickvdbosch@outlook.com>
2021-04-20 13:12:11 +0200

    Moved constants into their own class

---
+++ RvdB.Scrapionize/Constants.cs
@@ -0,0 +1,16 @@
+﻿namespace RvdB.Scrapionize
+{
+	internal static class Constants
+	{
+		internal const string DATE_FORMAT = "dd MMM yyyy";
+		internal const string CARRIAGERETURN_LINEFEED = "\r\n";
+		internal const string NAME_TITLE = "ibox-title";
+		internal const string NAME_CONTENT = "ibox-content";
+		internal const string NAME_HEADING2 = "h2";
+		internal const string NAME_HEADING3 = "h3";
+		internal const string NAME_HEADING4 = "h4";
+		internal const string NAME_HREF = "href";
+		internal const string NAME_LINK = "a";
+		internal const string NAME_SPAN = "span";
+	}
+}
